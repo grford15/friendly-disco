c46cb5bb36d0a0756f824ddad89fa639fe6a98df
Greg <g.rutherford10@gmail.com>
2020-03-04 12:03:05 +0000

    add simple form to send location

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="app">
-      <header>
-        <h1>Job Search</h1>
-      </header>
-      <body>
-        <h2>Search for jobs here</h2>
-      </body>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: '',
+      language: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      location: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('Submitted');
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header>
+          <h1>Junior Dev Job Search</h1>
+          <p>Find the perfect role right here</p>
+          <form onSubmit={this.handleSubmit}>
+            <label>Location</label>
+            <input
+              type="text"
+              placeholder="Search"
+              onChange={this.handleChange}
+            />
+            <button type="submit">Search</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
